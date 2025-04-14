da524e490b518ec62988e421280da9f5c49bb04a
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2025-04-14 13:55:30 +0900

    Initial Commit

---
+++ GO/GOApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOApp.swift
+//  GO
+//
+//  Created by 최승범 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
